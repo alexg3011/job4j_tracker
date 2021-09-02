d45c7b4ab633647ab98e020ec0858cfa461fc83e
Alex <psihicus@gmail.com>
2021-09-02 16:15:02 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/main/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.tracker;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ValidateInputTest {
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenTrueInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenMultiTrueInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"2", "0", "3", "4"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int[] expected = {2, 0, 3, 4};
+        int[] selected = new int[4];
+        for (int i = 0; i<4; i++) {
+            selected[i] = input.askInt("Enter menu:");
+        }
+        assertThat(selected, is(expected));
+    }
+
+    @Test (expected = NumberFormatException.class)
+    public void whenNegativeInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] { "-6"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        throw new NumberFormatException();
+    }
+}
