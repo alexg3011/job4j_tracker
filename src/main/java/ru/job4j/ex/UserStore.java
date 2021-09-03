9d39b480f8917527464f641ce4ffa10660adf5a5
Alex <psihicus@gmail.com>
2021-09-03 12:26:22 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #197974]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -4,38 +4,37 @@ public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
         User user = null;
         for (int i = 0; i < users.length; i++) {
-            if (users[i].getUsername().equals(login)){
+            if (users[i].getUsername().equals(login)) {
                 user = users[i];
+                break;
             }
         }
-        if (user==null) {
+        if (user == null) {
             throw new UserNotFoundException("Пользователь не найден");
         }
         return user;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (!user.isValid() || user.getUsername().length()<3){
+        if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("Пользователь не валидный");
         }
         return true;
     }
 
     public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
         try {
-            User[] users = {
-                    new User("Petr Arsentev", true)
-            };
-           User user = findUser(users, "Petr Arsentev");
+            User user = findUser(users, "Petr Arsentev");
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
-        }
-        catch (UserInvalidException e) {
+        } catch (UserInvalidException e) {
             e.printStackTrace();
+        } catch (UserNotFoundException ea) {
+            ea.printStackTrace();
         }
-       catch (UserNotFoundException ea) {
-           ea.printStackTrace();
-       }
     }
 }
