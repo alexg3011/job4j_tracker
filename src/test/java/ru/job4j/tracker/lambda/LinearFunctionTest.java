3861c1f1750952fa8d1e4b186b8a6bc6a05a3963
Alex <psihicus@gmail.com>
2021-09-14 19:29:26 +0300

    5. Подсчет функции в диапазоне. [#24251]

---
+++ src/test/java/ru/job4j/tracker/lambda/LinearFunctionTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker.lambda;
+import org.junit.Test;
+import ru.job4j.lambda.LinearFunction;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class LinearFunctionTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        LinearFunction function = new LinearFunction();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenQuadraticFunctionThenQuadraticResults() {
+        LinearFunction function = new LinearFunction();
+        List<Double> result = function.diapason(5, 8, x -> x * x + 2 * x + 1);
+        List<Double> expected = Arrays.asList(36D, 49D, 64D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenExpFunctionThenExpResults() {
+        LinearFunction function = new LinearFunction();
+        List<Double> result = function.diapason(5, 8, x -> Math.pow(x,x) + x * x + 2 * x + 1);
+        List<Double> expected = Arrays.asList(3161D, 46705.D, 823607D);
+        assertThat(result, is(expected));
+    }
+}
