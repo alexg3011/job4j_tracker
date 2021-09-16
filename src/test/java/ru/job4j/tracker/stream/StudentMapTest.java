1ca79d058318fe47fc4cbe9d7fb41b3bb62d0b00
Alex <psihicus@gmail.com>
2021-09-16 14:59:02 +0300

    0. Stream API улучшения [#58167]

--- src/test/java/ru/job4j/tracker/stream/StudentMapTest.java
+++ src/test/java/ru/job4j/tracker/stream/StudentMapTest.java
@@ -13,18 +13,18 @@ public class StudentMapTest {
     @Test
     public void whenListToMap() {
         List<Student> students = List.of(
-                new Student(10, "Surname1"),
-                new Student(40, "Surname4"),
-                new Student(50, "Surname1"),
-                new Student(70, "Surname7"),
-                new Student(90, "Surname1")
+                new Student("Surname1", 10),
+                new Student("Surname4", 40),
+                new Student("Surname1", 50),
+                new Student("Surname7", 70),
+                new Student("Surname1", 90)
         );
         StudentMap studentMap = new StudentMap();
         Map<String, Student> rsl = studentMap.studentToMap(students);
         Map<String, Student> expected = new HashMap<>();
-        expected.put("Surname1", new Student(10, "Surname1"));
-        expected.put("Surname4", new Student(40, "Surname4"));
-        expected.put("Surname7", new Student(70, "Surname7"));
+        expected.put("Surname1", new Student("Surname1", 10));
+        expected.put("Surname4", new Student("Surname4", 40));
+        expected.put("Surname7", new Student("Surname7", 70));
         assertThat(rsl, is(expected));
     }
 }
