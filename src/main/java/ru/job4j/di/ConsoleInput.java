59a9c8ae376fa3ecfc162eafa4d689963e937e52
Alex <psihicus@gmail.com>
2021-12-16 13:02:12 +0300

    0. Что такое DI? [#6876]

---
+++ src/main/java/ru/job4j/di/ConsoleInput.java
@@ -0,0 +1,20 @@
+package ru.job4j.di;
+
+import ru.job4j.tracker.Input;
+
+import java.util.Scanner;
+
+public class ConsoleInput implements Input {
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public String askStr(String question) {
+        System.out.print(question);
+        return scanner.nextLine();
+    }
+
+    @Override
+    public int askInt(String question) {
+        return Integer.parseInt(askStr(question));
+    }
+}
---
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -0,0 +1,30 @@
+package ru.job4j.di;
+
+public class StartUI {
+
+    private Store store;
+
+    private ConsoleInput input;
+
+    public StartUI(Store store) {
+        this.store = store;
+    }
+
+    public void add(String value) {
+        store.add(value);
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+
+    public String askStr(String question) {
+        return input.askStr(question);
+    }
+
+    public int askInt(String question) {
+        return input.askInt(question);
+    }
+}
