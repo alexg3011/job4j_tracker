0b27cf5c4a96e56c58498413e88ef9806ebc2224
Alex <psihicus@gmail.com>
2021-12-16 13:46:02 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -9,9 +9,14 @@ public class SpringDI {
         context.refresh();
         StartUI ui = context.getBean(StartUI.class);
         ConsoleInput console = context.getBean(ConsoleInput.class);
+        Store store = context.getBean(Store.class);
+        store.add("name1");
+        store.add("name2");
+        Store getStore = context.getBean(Store.class);
         ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
         ui.print();
         console.askStr("Hi!");
+        getStore.getAll().forEach(System.out::println);
     }
 }
